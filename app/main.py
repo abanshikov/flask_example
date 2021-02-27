71ee2aba9f0f2c7aeff11db05b9e0dc0981d3180
abanshikov <abanshikov@yandex.ru>
2021-02-27 16:33:39 +0700

    Create main.py

---
+++ app/main.py
@@ -0,0 +1,17 @@
+from flask import Flask
+try:
+    from config import Configuration
+except:
+    from .config import Configuration
+
+app = Flask(__name__)
+app.config.from_object(Configuration)
+
+
+@app.route("/")
+def index():
+    return f"<h1>Hello from flask!</h1>"
+
+
+if __name__ == "__main__":
+    app.run()
